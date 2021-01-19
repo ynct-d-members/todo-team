f7eba6a03f1f16e5634365e622ec98a0eaa08815
shippokun <okashi0tabetai@gmail.com>
2021-01-19 23:55:40 +0900

    refactor(libs/date): change export module
    dateを吐き出すように変更

--- src/libs/date/date.ts
+++ src/libs/date/date.ts
@@ -3,6 +3,10 @@ import "dayjs/locale/ja";
 
 dayjs.locale("ja");
 
-export const parseFormat = (data: Date, format = "YYYY/MM/DD HH:mm:ss") => {
+const format = (data: Date, format = "YYYY/MM/DD HH:mm:ss") => {
   return dayjs(data).format(format);
+};
+
+export const date = {
+  format,
 };
