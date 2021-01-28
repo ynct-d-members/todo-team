31e94f0dd86840a4fb0c5841243b8c46ba16d365
shippokun <okashi0tabetai@gmail.com>
2021-01-28 23:12:29 +0900

    feat: apiからデータを受け取るように変更

--- src/pages/todos/todo-detail/containers/todo-detail.container.tsx
+++ src/pages/todos/todo-detail/containers/todo-detail.container.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Todo } from "@prisma/client";
+
+import { http } from "../../../../libs/http";
 import { TodoDetail } from "../components";
 
+const initialState: Todo = {
+  id: 0,
+  title: "",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
 export const TodoDetailContainer: React.FC<{ id: number }> = (props) => {
-  return <TodoDetail id={1} />;
+  const [todo, setTodo] = useState(initialState);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await http.get<Todo>(`./todo/1`);
+      setTodo(result);
+    };
+    fetch();
+  }, [props.id]);
+
+  return <TodoDetail todo={todo} />;
 };
