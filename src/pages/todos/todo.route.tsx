18c86b5703cd326211ea5d129716dd0958a875a4
shippokun <okashi0tabetai@gmail.com>
2021-01-31 17:33:45 +0900

    feat: update todo-detail url

--- src/pages/todos/todo.route.tsx
+++ src/pages/todos/todo.route.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { TodoDetailContainer } from "./todo-detail";
+import { TodoDetailPage } from "./todo-detail";
 
 export const Routes: React.FC = () => {
   return (
     <>
       <Router>
         <div>
-          <Route exact path="/" component={TodoDetailContainer} />
+          <Route exact path="/todos/:id" component={TodoDetailPage} />
         </div>
       </Router>
     </>
