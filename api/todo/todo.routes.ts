7831b36214489197f11631d83bda45cef152923e
sunakane <20nm728a@vc.ibaraki.ac.jp>
2021-02-05 21:42:59 +0900

    style: rename /todo/ -> /todos

--- api/todo/todo.routes.ts
+++ api/todo/todo.routes.ts
@@ -4,7 +4,7 @@ import { TodoController } from "./todo.controller";
 export class TodoRoutes {
   initRoutes(server: FastifyInstance, opts: any, done: () => void) {
     const todoController = new TodoController();
-    server.get("/todo/", todoController.getTodosListHandler);
+    server.get("/todos", todoController.getTodosListHandler);
     server.get("/todo/:id", todoController.getTodoDetailHandler);
 
     done();
