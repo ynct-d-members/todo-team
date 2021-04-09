3a087c44366f3d849770e378b6289f8f3402b933
sunakane <20nm728a@vc.ibaraki.ac.jp>
2021-04-09 22:43:15 +0900

    test: getting undefined todo

--- api/modules/todo/todo.service.test.ts
+++ api/modules/todo/todo.service.test.ts
@@ -10,5 +10,10 @@ afterAll(async () => {
 test("TestTodoという名前のTodoを作成できる", async () => {
   const todoName = "TestTodo";
   const todo = await service.createTodo(todoName);
-  expect(todo.title).toMatch("TestTodo");
+  expect(todo.title).toMatch(todoName);
+});
+
+test("存在しない番号のTodoを取り出そうとするとnullが返ってくる", async () => {
+  const todo = await service.getTodo(2);
+  expect(todo).toBeNull();
 });
