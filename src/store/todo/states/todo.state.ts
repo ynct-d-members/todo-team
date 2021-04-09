d186def7f87930ff84627b4bdfdd0ac04fcd9627
shippokun <okashi0tabetai@gmail.com>
2021-04-09 22:42:42 +0900

    feat(store): selectedIdの追加

--- src/store/todo/states/todo.state.ts
+++ src/store/todo/states/todo.state.ts
@@ -6,10 +6,12 @@ export const featureKey = "todos";
 
 export interface TodoState extends EntityState<Todo> {
   isFetching: boolean;
+  selectedId: string | null;
 }
 
 export const adapter = createEntityAdapter<Todo>();
 
 export const initialState: TodoState = adapter.getInitialState({
   isFetching: false,
+  selectedId: null,
 });
