3794192b2e2176970b757c56692501644ba27fbb
shippokun <okashi0tabetai@gmail.com>
2021-04-19 22:18:53 +0900

    feat(todo-edit): add edit container

---
+++ src/pages/todos/todo-edit/containers/todo-edit.container.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Todo } from "@prisma/client";
+
+import { useTodoStore } from "@/store";
+import { TodoEdit, TodoUpdateDto } from "../components";
+
+import { http } from "@/libs/http";
+
+export const TodoEditContainer: React.FC<{ id: string }> = (props) => {
+  const { id } = props;
+  const history = useHistory();
+  const { fetch, todo } = useTodoStore();
+
+  const updateTodo = async (data: TodoUpdateDto) => {
+    http.patch<Todo>(`/todos/update/${id}`, data).then((res) => {
+      history.push(`/todos/${res.id}`);
+    });
+  };
+
+  useEffect(() => {
+    fetch({ id });
+  }, [id, fetch]);
+
+  return <TodoEdit todo={todo} submit={updateTodo} />;
+};
