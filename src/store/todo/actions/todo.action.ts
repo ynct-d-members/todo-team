dd782289aecef77f84f009b760d31bbe8aa238c3
shippokun <okashi0tabetai@gmail.com>
2021-04-27 23:03:20 +0900

    feat(store/todo): createとupdateとremoveを追加

--- src/store/todo/actions/todo.action.ts
+++ src/store/todo/actions/todo.action.ts
@@ -1,8 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import { Todo } from "@prisma/client";
 import { http } from "../../../libs/http";
 import { featureKey } from "../states";
+import { Todo, TodoCreateDto, TodoUpdateDto } from "@/models";
+import { todos } from "api/mocks/todo-mock";
 
 export const fetchAllTodos = createAsyncThunk(
   `${featureKey}/fetchAll`,
@@ -18,5 +19,32 @@ export const fetchTodo = createAsyncThunk(
     const { id } = arg;
     const result = await http.get<Todo>(`/todos/${id}`);
     return { todo: result };
+  }
+);
+
+export const createTodo = createAsyncThunk(
+  `${featureKey}/create`,
+  async (arg: { todo: TodoCreateDto }) => {
+    const { todo } = arg;
+    const result = await http.post<Todo>("/todos/new", todo);
+    return { todo: result };
+  }
+);
+
+export const updateTodo = createAsyncThunk(
+  `${featureKey}/update`,
+  async (arg: { id: string; todo: TodoUpdateDto }) => {
+    const { id, todo } = arg;
+    const result = await http.patch<Todo>(`/todo/${id}`, todo);
+    return { todo: result };
+  }
+);
+
+export const removeTodo = createAsyncThunk(
+  `${featureKey}/remove`,
+  async (arg: { id: string }) => {
+    const { id } = arg;
+    const result = await http.remove<Todo>(`/todo/${id}`);
+    return { id: result.id };
   }
 );
